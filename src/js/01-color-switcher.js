b0ddabc9cebd82d448807a321e048799b6013908
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2022-10-19 20:32:50 +0200

    d

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,19 +4,21 @@ const btnStop = document.querySelector('button[data-stop]');
 console.log(btnStart)
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 let setColorTimer
 const changeBodyColor = () => {
-  body.style.backgroundColor = `${getRandomHexColor()}`
+body.style.backgroundColor = `${getRandomHexColor()}`
 }
 btnStart.addEventListener('click', () => {
-  btnStart.setAttribute("disabled","disabled")
-  setColorTimer = setInterval(changeBodyColor, 1000)
+    btnStart.setAttribute("disabled","disabled")
+    setColorTimer = setInterval(changeBodyColor, 1000)
 })
 
 btnStop.addEventListener('click', () => {
-  clearInterval(setColorTimer)
-  btnStart.removeAttribute('disabled')
+    clearInterval(setColorTimer)
+    btnStart.removeAttribute('disabled')
 })
+
+
